113e24931a4be0eb58037ffdc15df5fd9e4c1149
Omari <student@10.0.0.64>
2016-02-22 19:47:09 -0500

    Code that is currently on competition bot
    - needs further testing

--- Bloodshot/src/org/usfirst/frc/team2415/robot/RobotMap.java
+++ Bloodshot/src/org/usfirst/frc/team2415/robot/RobotMap.java
@@ -9,21 +9,21 @@ public class RobotMap {
     public static final int LEFT_TALON_ZERO = 5;
     public static final int LEFT_TALON_ONE = 3;
     public static final int RIGHT_TALON_ZERO = 1;
-    public static final int RIGHT_TALON_ONE = 4;
+    public static final int RIGHT_TALON_ONE = 6;
     
     public static final int ROTATE_INTAKE_TALON = 2;
-    public static final int SPIN_INTAKE_TALON = 6;
+    public static final int SPIN_INTAKE_TALON = 4;
     
     public static final int CATAPULT_SOLENOID_1 = 0;
     public static final int CATAPULT_SOLENOID_2 = 1;
     
     public static final int[] FIRE_SOLENOIDS = {CATAPULT_SOLENOID_1, CATAPULT_SOLENOID_2};
     
-    public static final int[] RIGHT_ENCODER = {3,4};
-    public static final int[] LEFT_ENCODER = {5,6};
+    public static final int[] RIGHT_ENCODER = {0,1};
+    public static final int[] LEFT_ENCODER = {2,3};
     
-    public static final int[] INTAKE_ENCODER = {0,1};
-    public static final int INTAKE_STOPPER = 2;
+    public static final int[] INTAKE_ENCODER = {4,5};
+    public static final int INTAKE_STOPPER = 6;
     
     public static final int PCM_ID = 20;
     
